62f9aa5f939eaa7eee986356677c54bc57cd7e47
Simon N. <sini2500@student.miun.se>
2026-05-23 21:27:26 +0200

    cors origin

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(cors());
+
+app.use(cors({
+    origin: "https://https://dt207-2-sini2500.netlify.app/"
+}));
 
 const db = new sqlite3.Database("./db/cv.db");
 
